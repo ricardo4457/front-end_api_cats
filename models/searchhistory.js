c266843b8022cbc8e184963f710a5cd5c6233ec3
Ricardo Vieira <57718264+ricardo4457@users.noreply.github.com>
2025-03-27 13:42:58 +0000

    db : prepare search history model

--- models/searchhistory.js
+++ models/searchhistory.js
@@ -2,23 +2,25 @@
 const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class SearchHistory extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      this.hasMany(models.SearchResult, {
+        foreignKey: 'searchId',
+        as: 'results',
+      })
     }
   }
   SearchHistory.init(
     {
-      query: DataTypes.STRING,
-      userId: DataTypes.INTEGER,
+      query: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
     {
       sequelize,
       modelName: 'SearchHistory',
+      tableName: 'SearchHistories',
+      timestamps: true,
     },
   )
   return SearchHistory
