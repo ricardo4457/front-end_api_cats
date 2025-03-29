ba7d8b114abe7dd12ff3a9a5c8d83f1c2a6e6aa2
Ricardo Vieira <57718264+ricardo4457@users.noreply.github.com>
2025-03-29 17:06:24 +0000

    feat : create stats Store and acess the endpoints

---
+++ src/stores/statsStore.js
@@ -0,0 +1,66 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import axios from 'axios'
+
+export const useStatsStore = defineStore('StatsStore', () => {
+  const topQueries = ref([])
+  const topCategories = ref([])
+  const categoryCats = ref([])
+  const currentCategory = ref('')
+  const loading = ref(false)
+  const error = ref(null)
+
+  const fetchTopQueries = async () => {
+    try {
+      loading.value = true
+      const response = await axios.get('http://localhost:3000/api/v1/stats/top-queries')
+      topQueries.value = response.data
+    } catch (err) {
+      error.value = err.message
+    } finally {
+      loading.value = false
+    }
+  }
+
+  const fetchTopCategories = async () => {
+    try {
+      loading.value = true
+      const response = await axios.get('http://localhost:3000/api/v1/stats/top-categories')
+      topCategories.value = response.data
+    } catch (err) {
+      error.value = err.message
+    } finally {
+      loading.value = false
+    }
+  }
+
+  const fetchCatsByCategory = async (tag) => {
+    try {
+      loading.value = true
+      currentCategory.value = tag
+      const response = await axios.get(
+        `http://localhost:3000/api/v1/stats/category/${encodeURIComponent(tag)}`,
+      )
+      categoryCats.value = response.data.cats
+    } catch (err) {
+      error.value = err.message
+    } finally {
+      loading.value = false
+    }
+  }
+
+  return {
+    //State
+    topQueries,
+    topCategories,
+    categoryCats,
+    currentCategory,
+    loading,
+    error,
+
+    // Actions
+    fetchTopQueries,
+    fetchTopCategories,
+    fetchCatsByCategory,
+  }
+})
