03b365f17a80eb13cc7b696ce25f546229b1b9ac
Ricardo Vieira <57718264+ricardo4457@users.noreply.github.com>
2025-04-12 15:11:24 +0100

    dev : config vite to redirect and use front-end server ip

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,19 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+
 export default defineConfig({
+  server: {
+  host: true,  
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      clientPort: 5173 
+    }
+  },
   plugins: [
-    vue(),
-    vueDevTools(),
+    [vue()],
+    [vueDevTools()],
   ],
   resolve: {
     alias: {
